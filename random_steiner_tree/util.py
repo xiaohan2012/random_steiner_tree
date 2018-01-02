f2dc54f0b491e89129192483c4d65cb84f0f5346
Han Xiao <han.xiao@aalto.fi>
2018-01-02 12:57:55 +0000

    converter from graph_tool.Graph

--- random_steiner_tree/util.py
+++ random_steiner_tree/util.py
@@ -19,3 +19,31 @@ def nx2edges_and_weights(g, weight='weight'):
 def from_nx(g, weight='weight'):
     return build_graph(g.number_of_nodes(),
                        nx2edges_and_weights(g, weight))
+
+
+def gt2edges_and_weights(g, weights):
+    """
+    args:
+    g: graph_tool.Graph
+    weights: list of edge weight, edge order should be the same g.get_edges()
+    returns:
+    
+    list of (source, target, weight)
+    """
+    from graph_tool import PropertyMap
+    if isinstance(weights, PropertyMap):  # convert to np.ndarray if necessary
+        weights = weights.a
+    elif weights is None:
+        weights = [1] * g.num_edges()
+
+    assert g.num_edges() == len(weights)
+
+    return [(e[0], e[1], w)
+            for e, w in zip(g.get_edges(), weights)]
+
+
+def from_gt(g, weights=None):
+    """weights: edge weights, PropertyMap or np.ndarray
+    """
+    return build_graph(g.num_vertices(),
+                       gt2edges_and_weights(g, weights))
--- test.py
+++ test.py
@@ -2,8 +2,9 @@ import pytest
 import random
 import numpy as np
 import networkx as nx
+from graph_tool import Graph
 from random_steiner_tree import random_steiner_tree
-from random_steiner_tree.util import from_nx
+from random_steiner_tree.util import from_nx, from_gt
 
 
 def check_feasiblility(tree, root, X):
@@ -22,36 +23,43 @@ def check_feasiblility(tree, root, X):
             assert n in X, 'one leaf does not belong to terminal'
 
 
-@pytest.fixture
-def input_data():
+def input_data_nx():
     g = nx.karate_club_graph()
     for u, v in g.edges_iter():
         g[u][v]['weight'] = 1
 
-    return g, from_nx(g, 'weight')
+    return g, from_nx(g, 'weight'), g.number_of_nodes()
 
 
-def test_loop_erased_feasiblility(input_data):
-    g, gi = input_data
-    for i in range(10):
-        # try different number of terminals
-        for k in range(2, g.number_of_nodes()+1):
-            X = np.random.permutation(g.number_of_nodes())[:10]
-            root = random.choice(g.nodes())
-            tree_edges = random_steiner_tree(gi, X, root, method='loop_erased')
-            t = nx.Graph()
-            t.add_edges_from(tree_edges)
-            check_feasiblility(t, root, X)
+def input_data_gt():
+    g_nx = nx.karate_club_graph()
+    g = Graph(directed=False)
+    g.add_vertex(g_nx.number_of_nodes())
+    for u, v in g_nx.edges():
+        g.add_edge(u, v)
+
+    return g, from_gt(g, None), g.num_vertices()
 
 
+@pytest.mark.parametrize("data_type", ["nx", "gt"])
 @pytest.mark.parametrize("method", ["loop_erased", "cut"])
-def test_feasiblility(input_data, method):
-    g, gi = input_data
+def test_feasiblility(data_type, method):
+    if data_type == 'nx':
+        data = input_data_nx()
+    elif data_type == 'gt':
+        data = input_data_gt()
+    g, gi, N = data
     for i in range(10):
         # try different number of terminals1
-        for k in range(2, g.number_of_nodes()+1):
-            X = np.random.permutation(g.number_of_nodes())[:10]
-            root = random.choice(g.nodes())
+        for k in range(2, N+1):
+            X = np.random.permutation(N)[:10]
+
+            if data_type == 'nx':
+                nodes = g.nodes()
+            elif data_type == 'gt':
+                nodes = list(map(int, g.vertices()))
+
+            root = random.choice(nodes)
             tree_edges = random_steiner_tree(gi, X, root, method=method)
             t = nx.Graph()
             t.add_edges_from(tree_edges)
